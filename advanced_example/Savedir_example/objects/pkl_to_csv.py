adb3fdcb07f693309f3649135c90e97cfd01d567
sijinwnag <sijin.wang@student.unsw.edu.au>
2022-01-21 16:00:12 +1100

    multi

--- advanced_example/Savedir_example/objects/pkl_to_csv.py
+++ advanced_example/Savedir_example/objects/pkl_to_csv.py
@@ -1,6 +1,6 @@
 import pickle as pkl
 import pandas as pd
-with open(r"C:\Users\sijin wang\Documents\GitHub\Yoann_code\advanced_example\Savedir_example\objects\ltsDF_ID0_2022-01-21-15-36-47.pkl", "rb") as f:
+with open(r"C:\Users\sijin wang\Documents\GitHub\Yoann_code\advanced_example\Savedir_example\objects\ltsDF_ID0_2022-01-21-15-59-01.pkl", "rb") as f:
     object = pkl.load(f)
 
 df = pd.DataFrame(object)
